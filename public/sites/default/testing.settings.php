293bb70fcdf687b8ff0bbc6026637b23696a1ca3
tuutti <tuutti@iki.fi>
2024-02-13 11:08:51 +0200

    UHF-9546: New test and staging addresses

--- public/sites/default/testing.settings.php
+++ public/sites/default/testing.settings.php
@@ -1,11 +1,3 @@
 <?php
 
-$config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/test-yritykset-ja-tyo/openid-connect/tunnistamo';
-$config['helfi_proxy.settings']['asset_path'] = 'test-elo-assets';
-$config['helfi_proxy.settings']['prefixes'] = [
-  'en' => 'test-business-and-work',
-  'fi' => 'test-yritykset-ja-tyo',
-  'sv' => 'test-foretag-och-arbete',
-  'ru' => 'test-business-and-work',
-  'zxx' => 'test-business-and-work',
-];
+$config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/yritykset-ja-tyo/openid-connect/tunnistamo';
